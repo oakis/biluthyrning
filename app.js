77d54a699aa794615c9e32a7e525cbf3482b57f2
Anders Ekman <oakismen@gmail.com>
2016-02-29 16:57:21 +0100

    Added empty jade templates

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var boka = require('./routes/boka');
+var fordon = require('./routes/fordon');
+var funktioner = require('./routes/funktioner');
 
 var app = express();
 
@@ -24,6 +27,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/boka', boka);
+app.use('/fordon', fordon);
+app.use('/funktioner', funktioner);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
