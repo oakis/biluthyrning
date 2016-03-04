b8576c7a5f20ab5e6ae018b3ca78db41e688ccfa
Anders Ekman <oakismen@gmail.com>
2016-03-04 09:09:56 +0100

    Bugfix, Added sort array by key function, started on update function

--- routes/funktioner.js
+++ routes/funktioner.js
@@ -13,6 +13,17 @@ checkIfExist = function (req, db) {
   return isItTrue;
 }
 
+sortArr = function (key) {
+	return function(a, b){
+		var valA=a[key].toLowerCase(), valB=b[key].toLowerCase();
+			if (valA < valB)
+				return -1;
+			if (valA > valB)
+				return 1;
+			return 0;
+		};
+}
+
 
 
 /* GET home page. */
@@ -23,16 +34,9 @@ router.get('/', function(req, res, next) {
 		data = data.toString();
 		var arr = data.split('*');
 		arr.forEach(function(v,i){
-			funkArr.push(JSON.parse(arr[i]))
+			funkArr.push(JSON.parse(arr[i]));
 		})
-		funkArr.sort(function(a, b){
-		var nameA=a.name.toLowerCase(), nameB=b.name.toLowerCase();
-			if (nameA < nameB) //sort string ascending
-				return -1;
-			if (nameA > nameB)
-				return 1;
-			return 0; //default return value (no sorting)
-		});
+		funkArr.sort(sortArr('name')); // Sortera efter 'name' innan arrayen skickas till render
 		res.render('funktioner', {funklista: funkArr});
 	})
 });
@@ -47,17 +51,10 @@ router.post('/add', function(req, res, next) {
 			funkArr.push(JSON.parse(arr[i]))
 		})
 		var newFunktion = {
-			"id": funkArr.length + 1,
+			"id": funkArr[funkArr.length - 1].id + 1, // Titta på sista element.id och lägg till 1.
 			"name": req.body.name
 		}
-		funkArr.sort(function(a, b){
-		var nameA=a.name.toLowerCase(), nameB=b.name.toLowerCase();
-			if (nameA < nameB) //sort string ascending
-				return -1;
-			if (nameA > nameB)
-				return 1;
-			return 0; //default return value (no sorting)
-		});
+		funkArr.sort(sortArr('name')); // Sortera efter 'name' innan arrayen skickas till render
 		var send = '\n*\n' + JSON.stringify(newFunktion,null,"\t");
 		if (checkIfExist(newFunktion, funkArr) == true) {
 			console.log('Funktionen finns redan registrerad')
@@ -75,5 +72,28 @@ router.post('/add', function(req, res, next) {
 		}
 	})
 });
+
+router.post('/update', function(req,res,next){
+	var funkArr = [];
+	fs.readFile(funktioner, 'utf8', (err, data) => {
+    if (err) throw err
+    data = data.toString();
+		var arr = data.split('*');
+		arr.forEach(function(v,i){
+			funkArr.push(JSON.parse(arr[i]))
+		})
+
+		/* Om ett id är 'checked' så ska objektet som innehåller id tas bort.
+		   Om ett id's value inte stämmer överens med id's name så ska id tas bort
+		   samt läggas till på nytt med det nya namnet(value)*/
+
+    /*var send = '\n*\n' + JSON.stringify(newFunktion,null,"\t");
+    fs.writeFile(funktioner, send, function(err) {
+      if (err) throw err;
+      res.render('funktioner', { funklista: funkArr });
+      console.log("File saved");
+    });*/
+  });
+})
 
 module.exports = router;
