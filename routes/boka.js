677a1202d626a5259d8b5634b6231a7358616c24
Anders Ekman <oakismen@gmail.com>
2016-03-09 14:33:28 +0100

    bokningar progress

--- routes/boka.js
+++ routes/boka.js
@@ -29,6 +29,7 @@ function tillvalFix (req) { // Om bara ett tillval, gör om till en array med 1 
 function giveMeCar (needs,db,bokningar) {
 	var regnummer;
 	var valjBastBil = [];
+	var valjBastBil_next_step = [];
 	db.forEach(function(v,i){
 		if (db[i].type == needs.type) { // kontrollera typ av bil
 			var carMatch = '';
@@ -47,32 +48,38 @@ function giveMeCar (needs,db,bokningar) {
 	});
 	// Kontrollera att ingen bokning på passande bilar finns vid valt datum.
 	valjBastBil.forEach(function(v,i){
+		var carMatch = '';
+		console.log('------------ valjBastBil loop start ------------')
+		bokningar.forEach(function(val,ind){
+			console.log('<<<<<<<<<<<<< bokningar loop start >>>>>>>>>>>>>>>')
+			// Om regnum i valjBastBil[i] finns i bokningar[ind].regnum, kontrollera tider
+			if (valjBastBil[i] == bokningar[ind].regnum) {
 
-			// --- user choose date from
-			var user_from_year = needs.franDatum.substring(0,4);
-			var user_from_month = needs.franDatum.substring(5,7) -1;
-			var user_from_day = needs.franDatum.substring(8,10);
-			var user_from_full_date = moment().set({'year': user_from_year,'month': user_from_month,'date': user_from_day});
+				// --- user choose date from
+				var user_from_year = needs.franDatum.substring(0,4);
+				var user_from_month = needs.franDatum.substring(5,7) -1;
+				var user_from_day = needs.franDatum.substring(8,10);
+				var user_from_full_date = moment().set({'year': user_from_year,'month': user_from_month,'date': user_from_day});
 
-			// --- booked date from
-			var book_from_year = bokningar[i].franDatum.substring(0,4);
-			var book_from_month = bokningar[i].franDatum.substring(5,7) -1;
-			var book_from_day = bokningar[i].franDatum.substring(8,10);
-			var book_from_full_date = moment().set({'year': book_from_year,'month': book_from_month,'date': book_from_day});
+				// --- booked date from
+				var book_from_year = bokningar[ind].franDatum.substring(0,4);
+				var book_from_month = bokningar[ind].franDatum.substring(5,7) -1;
+				var book_from_day = bokningar[ind].franDatum.substring(8,10);
+				var book_from_full_date = moment().set({'year': book_from_year,'month': book_from_month,'date': book_from_day});
 
-			// --- user date to
-			var user_to_year = needs.tillDatum.substring(0,4);
-			var user_to_month = needs.tillDatum.substring(5,7) -1;
-			var user_to_day = needs.tillDatum.substring(8,10);
-			var user_to_full_date = moment().set({'year': user_to_year,'month': user_to_month,'date': user_to_day});
+				// --- user date to
+				var user_to_year = needs.tillDatum.substring(0,4);
+				var user_to_month = needs.tillDatum.substring(5,7) -1;
+				var user_to_day = needs.tillDatum.substring(8,10);
+				var user_to_full_date = moment().set({'year': user_to_year,'month': user_to_month,'date': user_to_day});
 
-			// --- booked date tom
-			var book_to_year = bokningar[i].tillDatum.substring(0,4);
-			var book_to_month = bokningar[i].tillDatum.substring(5,7) -1;
-			var book_to_day = bokningar[i].tillDatum.substring(8,10);
-			var book_to_full_date = moment().set({'year': book_to_year,'month': book_to_month,'date': book_to_day});
+				// --- booked date tom
+				var book_to_year = bokningar[ind].tillDatum.substring(0,4);
+				var book_to_month = bokningar[ind].tillDatum.substring(5,7) -1;
+				var book_to_day = bokningar[ind].tillDatum.substring(8,10);
+				var book_to_full_date = moment().set({'year': book_to_year,'month': book_to_month,'date': book_to_day});
 
-			// --------------- user choose time start---------
+				// --------------- user choose time start---------
 				// --- user choose from time
 				var user_from_time = needs.franTid.substring(0,2);
 				var user_from_min = needs.franTid.substring(3,7);
@@ -82,31 +89,52 @@ function giveMeCar (needs,db,bokningar) {
 				var user_to_min = needs.tillTid.substring(3,7);
 				var user_to_full_time = moment().set({'hour':user_to_time, 'minute':user_to_min});
 
-
 				// --- booked  from time
-				var booked_from_time = bokningar[i].franTid.substring(0,2);
-				var booked_from_min = bokningar[i].franTid.substring(3,7);
+				var booked_from_time = bokningar[ind].franTid.substring(0,2);
+				var booked_from_min = bokningar[ind].franTid.substring(3,7);
 				var booked_from_full_time = moment().set({'hour':booked_from_time, 'minute':booked_from_min});
 				// --- booked  to time
-				var booked_to_time = bokningar[i].tillTid.substring(0,2);
-				var booked_to_min = bokningar[i].tillTid.substring(3,7);
+				var booked_to_time = bokningar[ind].tillTid.substring(0,2);
+				var booked_to_min = bokningar[ind].tillTid.substring(3,7);
 				var booked_to_full_time = moment().set({'hour':booked_to_time, 'minute':booked_to_min});
-			// --------------- user choose time end---------
+				// --------------- user choose time end---------
+				/*function isPrivat () {
+					if (needs.privat == 'on') {
+						if (moment(user_from_full_date).format('dddd') != 'Saturday' ||
+								moment(user_from_full_date).format('dddd') != 'Sunday' ||
+							  moment(user_to_full_date).format('dddd') != 'Saturday' ||
+								moment(user_to_full_date).format('dddd') != 'Sunday') {
+							  // ^ Om det inte är helg - Kontrollera att bokning går utanför arbetstid
 
+						} else {
+							console.log('Bokning är inte på helgdag')
+						}
+					}	
+				}*/
 
-			if (user_from_full_date.isSameOrAfter(book_from_full_date) && user_to_full_date.isSameOrBefore(book_to_full_date)) {
-				if (user_from_full_time.isSameOrAfter(booked_from_full_time) && user_to_full_time.isSameOrBefore(booked_to_full_time)) {
-					console.log(valjBastBil[i] + ' går att boka!!!!!!!!!!!!!!!!!');
+				if (user_from_full_date.isSameOrAfter(book_from_full_date) && user_to_full_date.isSameOrBefore(book_to_full_date)) {
+					if (user_from_full_time.isSameOrAfter(booked_from_full_time) && user_to_full_time.isSameOrBefore(booked_to_full_time)) {
+						carMatch += 'y';
+					} else {
+						carMatch += 'n';
+					};
 				} else {
-					console.log(valjBastBil[i] + ' går EJ att boka');
-				};
-			} else {
-				console.log(valjBastBil[i] + ' går att boka');
+					carMatch += 'y';
+				}
 			}
+		});
+		console.log(carMatch);
+		if (carMatch.indexOf('n') == -1) { // om carMatch inte inne håller något n så sparas bilen i en array
+				valjBastBil_next_step.push(valjBastBil[i]);
+		}
 	});
-	console.log('\nEfter datum/tid kontroll:\n'+valjBastBil+'\n');
+	console.log(valjBastBil_next_step);
+	//console.log('\nEfter datum/tid kontroll:\n'+valjBastBil+'\n');
+	//console.log(needs.privat)
 	//console.log(regnummer);
 }
+
+
 
 /*
  Lagra alla bilar som inte är bokade valt datum, sortera efter när den användes senast och
